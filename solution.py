dde4a07ad7103d8a70b6db1bfb6a86b6b969adfb
Alexandre Cho <alecho97@gmail.com>
2025-02-05 23:54:14 -0300

    Refactor tuple counting logic for improved efficiency

--- solution.py
+++ solution.py
@@ -5,14 +5,16 @@ class Solution:
         if len(nums) < 4:
             return 0
 
-        nums.sort()
-        validTuples = 0
+        product_count = {}
+        valid_tuples = 0
+
         for i in range(len(nums)):
-            for j in range(len(nums)-1,i,-1):
-                for k in range(i+1,j):
-                    for l in range(j-1,k,-1):
-                        print(i,j,k,l)
-                        if nums[i]*nums[j] == nums[k]*nums[l]:
-                            validTuples += 8
-                
-        return validTuples
+            for j in range(i + 1, len(nums)):
+                product = nums[i] * nums[j]
+                if product in product_count:
+                    valid_tuples += product_count[product]
+                    product_count[product] += 1
+                else:
+                    product_count[product] = 1
+
+        return valid_tuples * 8
